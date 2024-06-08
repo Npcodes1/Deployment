f9473d6cca8b62cea07eaa97a56ae5ff5ba069d7
Nicole Payne <nicole1rock@gmail.com>
2024-06-08 12:37:47 -0500

    updated PORT to 8080

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //lets the app know to look for the dotenv node modules folder, find the config file, and run the method. Allows us to load environmental variables from .env file
-require("dotenv").config(); 
+require("dotenv").config();
 
 //to use the connection file
 require("./config/connection");
@@ -9,10 +9,10 @@ require("./config/authStrategy");
 
 //Middleware- express and morgan
 const express = require("express");
-const morgan = require ("morgan");
+const morgan = require("morgan");
 
 //imports path module - gives the capability to create a path.
-const path = require("node:path"); 
+const path = require("node:path");
 
 //require cors
 const cors = require("cors");
@@ -29,8 +29,8 @@ const session = require("express-session");
 //Create a const variable called app with the value of express()
 const app = express();
 
-//Create a const variable called PORT with the value of 3000
-const PORT = process.env.PORT || 3000;
+//Create a const variable called PORT with the value of 8080
+const PORT = process.env.PORT || 8080;
 
 //define routing variables to create a path to bookRoutes and authRoutes
 const bookRoutes = require("./routes/bookRoutes");
@@ -44,24 +44,24 @@ app.use(helmet({ contentSecurityPolicy: false }));
 app.use(morgan("dev"));
 
 // use cors- only allows certain servers inside access(VIP bouncer)
-app.use(cors()); 
+app.use(cors());
 
 // JSON
 app.use(express.json());
 
 //helps parse form data
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //to provide path to public directory
-app.use(express.static(path.join(__dirname + "/public"))); 
+app.use(express.static(path.join(__dirname + "/public")));
 
 //use session
 app.use(
-    session({
+  session({
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: false,
-})
+  })
 );
 
 //initialize passport
@@ -72,37 +72,62 @@ app.use(passport.session());
 
 //Routing Paths
 //use the route path for bookRoutes and authRoutes
-app.use("/api/books", bookRoutes); 
+app.use("/api/books", bookRoutes);
 app.use("/", authRoutes);
 
 // GET routes to other pages- will remove later
 //points to the Home page
 app.get("/", (req, res, next) => {
-    res.status(200).json({success: {message: "This route points to the Home page"}, statusCode: 200});
+  res
+    .status(200)
+    .json({
+      success: { message: "This route points to the Home page" },
+      statusCode: 200,
+    });
 });
 
 //points to the About page"
 app.get("/about", (req, res, next) => {
-    res.status(200).json({success: {message: "This route points to the About page"}, statusCode: 200});
+  res
+    .status(200)
+    .json({
+      success: { message: "This route points to the About page" },
+      statusCode: 200,
+    });
 });
 
 //points to the Login page"
 app.get("/login", (req, res, next) => {
-    res.status(200).json({success: {message: "This route points to the Login page"}, statusCode: 200});
+  res
+    .status(200)
+    .json({
+      success: { message: "This route points to the Login page" },
+      statusCode: 200,
+    });
 });
 
 //points to the Admin Console page"
 app.get("/admin", (req, res, next) => {
-    res.status(200).json({success: {message: "This route points to the Admin Console page"}, statusCode: 200});
+  res
+    .status(200)
+    .json({
+      success: { message: "This route points to the Admin Console page" },
+      statusCode: 200,
+    });
 });
 
 //points to the Create Book page"
 app.get("/admin/create-book", (req, res, next) => {
-    res.status(200).json({success: {message: "This route points to the Create Book page"}, statusCode: 200});
+  res
+    .status(200)
+    .json({
+      success: { message: "This route points to the Create Book page" },
+      statusCode: 200,
+    });
 });
 
 //Server
 app.listen(PORT, () => {
-console.log(`The server is listening on port ${PORT}`);
-console.log(`http://localhost:${PORT}`);
+  console.log(`The server is listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
--- controllers/authController.js
+++ controllers/authController.js
@@ -6,57 +6,83 @@ const User = require("../models/userModel");
 
 //loginLocalFailed
 const loginLocalFailed = (req, res, next) => {
-    res.status(401).json({ error: { message: "Username or password is incorrect." }, statusCode: 401 });
+  res
+    .status(401)
+    .json({
+      error: { message: "Username or password is incorrect." },
+      statusCode: 401,
+    });
 };
 
 //logoutRequest
 const logoutRequest = (req, res, next) => {
-    req.logout((error) => {
-        //if there's an error, show the error
-        if(error) {
-            res.status(400).json({ error: { message: "Something went wrong!" }, statusCode: 400 })
-        } //otherwise if there's no error, show user successfully logged out
-        res.status(200).json({ success: { message: "User logged out!" }, statusCode: 200 });
-        
-    });
+  req.logout((error) => {
+    //if there's an error, show the error
+    if (error) {
+      res
+        .status(400)
+        .json({ error: { message: "Something went wrong!" }, statusCode: 400 });
+    } //otherwise if there's no error, show user successfully logged out
+    res
+      .status(200)
+      .json({ success: { message: "User logged out!" }, statusCode: 200 });
+  });
 };
 
 //signupRequest
 const signupRequest = (req, res, next) => {
-    const { firstName, lastName, username, password } = req.body;
-    bcrypt.hash(password, 10, (error, hashedPassword) => {
-        //return next error
-        if(error) {
-            return next(error);
-        }
+  const { firstName, lastName, username, password } = req.body;
+  bcrypt.hash(password, 10, (error, hashedPassword) => {
+    //return next error
+    if (error) {
+      return next(error);
+    }
 
-        //create new user using the User model
-        const newUser = new User({
-            firstName,
-            lastName,
-            username,
-            password: hashedPassword
+    //create new user using the User model
+    const newUser = new User({
+      firstName,
+      lastName,
+      username,
+      password: hashedPassword,
+    });
+
+    try {
+      newUser.save();
+      req.login(newUser, (err) => {
+        //check for errors before creating new user
+        if (err) {
+          res
+            .status(400)
+            .json({
+              error: { message: "Something went wrong while signing up!" },
+              statusCode: 400,
+            });
+        }
+      }); //if no errors, then create new user using the data from User (minus password)
+      res
+        .status(201)
+        .json({
+          success: { message: "New user is created" },
+          data: { firstName, lastName, username },
+          statusCode: 201,
         });
 
-        try {
-            newUser.save();
-            req.login(newUser, (err) => {
-                //check for errors before creating new user
-                if(err) {
-                    res.status(400).json({ error: { message: "Something went wrong while signing up!" }, statusCode: 400 });
-                } 
-            }) //if no errors, then create new user using the data from User (minus password)
-            res.status(201).json({ success: {message: "New user is created"}, data: { firstName, lastName, username }, statusCode: 201});
-
-        //check for errors again after creating user
-        } catch (error) {
-            //if there's a duplicate username
-            if(error.code === 11000, error.keyPattern.username) {
-                res.status(400).json({ error: { message: "Username already exists" }, statusCode: 400});
-            } //else there's a problem with the server and the request can't be made
-            res.status(500).json({ error: { message: "Internal server error" }, statusCode: 500});
-        }
-    });
+      //check for errors again after creating user
+    } catch (error) {
+      //if there's a duplicate username
+      if ((error.code === 11000, error.keyPattern.username)) {
+        res
+          .status(400)
+          .json({
+            error: { message: "Username already exists" },
+            statusCode: 400,
+          });
+      } //else there's a problem with the server and the request can't be made
+      res
+        .status(500)
+        .json({ error: { message: "Internal server error" }, statusCode: 500 });
+    }
+  });
 };
 
 module.exports = { loginLocalFailed, logoutRequest, signupRequest };
